7dcb0b4c1332080b529cef2384bb1b7c429c8797
Osaulenko, Egor - PSC <egor.osaulenko@wisconsin.gov>
2025-05-21 15:35:17 -0500

    init commit;

---
+++ README.md
@@ -0,0 +1,12 @@
+# Url's
+
+# Framework
+Application is build with ASP.NET Core MVC Framework
+
+# Client
+Personal project
+
+# Introduction 
+
+
+# Reasoning
---
+++ xamplTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xampl.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xampl.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void HomeControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PrivacyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ErrorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
