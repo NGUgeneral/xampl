2589aac783a4240e80c5456e0d9254ab46e2181d
Egor Osaulenko <egor.osaulenko@gmail.com>
2025-07-02 14:28:17 -0500

    XMP-59: introduce Admin section;

---
+++ xampl/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xampl.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
