f05df70d305aad0852f0724a751757c7f592cddd
Egor Osaulenko <egor.osaulenko@gmail.com>
2025-07-21 13:59:01 -0500

    XMP-69: authorize User section;

--- xampl/Controllers/UsersController.cs
+++ xampl/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using xampl.Models.Documents;
@@ -8,6 +9,7 @@ using xampl.ViewModels;
 
 namespace xampl.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UsersController(
         IRepository<DocumentsContext> documentsRepository,
         IMapper mapper,
